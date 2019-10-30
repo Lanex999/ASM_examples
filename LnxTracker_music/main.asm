0221a8986359dfed5a0b6228a5f87c28c245b149
Lanex999 <lanex44@gmail.com>
2019-10-30 20:13:20 +0100

    LnxTracker example

---
+++ LnxTracker_music/main.asm
@@ -0,0 +1,17 @@
+; LnxSpectrum ASM editor (www.ilnx.cz) 30.10.2019
+
+	org	32768
+	
+	ld	de,music
+	call	mus.LoadPlay
+	
+	ei
+loop	call	mus.int_Patterns
+	call	mus.int_SAA
+	halt
+	jr	loop	
+	
+	include	"LnxTrackerEngine.slb"	
+	
+music	include	"SofTB_SAA.bin"
+	
